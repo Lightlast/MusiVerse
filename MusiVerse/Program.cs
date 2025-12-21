3d5435493978dc1374ea8c5f1d52702aed9bfc43
Nguyễn Huy nghiêm <teonguyen12111@gmail.com>
2025-12-21 11:53:53 +0700

    base code cơ bản

--- MusiVerse/Program.cs
+++ MusiVerse/Program.cs
@@ -16,7 +16,7 @@ namespace MusiVerse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
