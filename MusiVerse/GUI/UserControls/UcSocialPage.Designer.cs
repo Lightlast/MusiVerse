f4f5c29453d6831558697b0e3c1e9e6ec5d72ec2
Nguyễn Huy nghiêm <teonguyen12111@gmail.com>
2026-01-08 11:13:50 +0700

    xóa vài thứ ko cần thiết

--- MusiVerse/GUI/UserControls/UcSocialPage.Designer.cs
+++ MusiVerse/GUI/UserControls/UcSocialPage.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace MusiVerse.GUI.Forms.Social
+﻿namespace MusiVerse.GUI.UserControls
 {
-    partial class frmProfile
+    partial class UcSocialPage
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,19 +20,16 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 800);
-            this.Text = "User Profile";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
         }
 
         #endregion
